5a0a3904ec5a097a879ef27c4cd2af947970ab3d
Stefano <stefanodpc29@gmail.com>
2026-03-23 22:56:16 +0000

    chore: add errors and isolated DI

--- src/infrastructure/dependencies/user_dependencies.py
+++ src/infrastructure/dependencies/user_dependencies.py
@@ -3,11 +3,10 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
+from src.domains.users.repository import IPasswordHasher, IUserRepository
+from src.domains.users.service import UserService
 from src.infrastructure.database import get_db
 from src.infrastructure.repositories.user_repository import PasswordHasherImpl, UserRepositoryImpl
-
-from .repository import IPasswordHasher, IUserRepository
-from .service import UserService
 
 DatabaseSession = Annotated[Session, Depends(get_db)]
 
